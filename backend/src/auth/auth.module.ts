91c7825dcea50bcc97f73e337ecdb13b6557e95d
sergey56431 <sergey56431@gmail.com>
2024-12-22 21:00:38 +0600

    Начало работы с jwt

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,8 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma';
 import { UsersService } from '../users';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, PrismaService, UsersService],
 })
