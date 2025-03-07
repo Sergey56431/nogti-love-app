e78acfef3f2d07a0514f9afb692fb1d407419725
RokinR <atvg@inbox.ru>
2025-03-07 23:54:27 +0600

    Запросы на Получение, Обновление и Удаление 2.0

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-  import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma';
@@ -11,7 +11,7 @@ import { IncomExpencesModule } from './incom_expences';
 import { CategoriesModule } from './categories';
 import { ServicesModule } from './services';
 import { SettingsModule } from './settings/settings.module';
-  import { CategoryOperationsModule } from './categoryOperations';
+import { CategoryOperationsModule } from './categoryOperations';
 
 @Module({
   imports: [
