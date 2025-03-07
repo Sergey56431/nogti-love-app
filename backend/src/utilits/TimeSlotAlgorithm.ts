93083fcc7f444572d59ff6d12caf5d3c39d46fd4
aleksey10832 <akuratov47@gmail.com>
2025-03-07 15:20:59 +0600

    внёс правки, использовал padStart()

--- backend/src/utilits/TimeSlotAlgorithm.ts
+++ backend/src/utilits/TimeSlotAlgorithm.ts
@@ -11,7 +11,7 @@ interface UserSettings {
 export class TimeSlotAlgorithm {
   constructor(
     private readonly _servicesService: ServicesService,
-    readonly _settingsService: SettingsService,
+    private readonly _settingsService: SettingsService,
   ) {}
 
   private async _convertTimeToMinutes(time: string): Promise<number> {
@@ -19,14 +19,7 @@ export class TimeSlotAlgorithm {
     return ((+time1[0]) * 60) + (+time1[1]);
   }
   private async _formatMinutesToTime(minutes: number): Promise<string> {
-    const time: string[] = [`${Math.floor(minutes / 60)}`, `${minutes % 60}`];
-    if (Math.floor(minutes / 60) < 10) {
-      time[0] = `0${Math.floor(minutes / 60)}`;
-    }
-    if (minutes % 60 < 10) {
-      time[1] = `0${minutes % 60}`;
-    }
-    return `${time[0]}:${time[1]}`;
+    return `${`${Math.floor(minutes / 60)}`.padStart(2, '0')}:${`${minutes % 60}`.padStart(2, '0')}`;
   }
   private async _getServicesDuration(serviceIds: string[]): Promise<number> {
     let alltime: number = 0;
