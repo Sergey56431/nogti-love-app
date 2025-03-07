53a7712c05aa2d6ebcf02adc7dfdc661c7bd291c
sergey56431 <sergey56431@gmail.com>
2025-03-07 20:27:48 +0600

    Переработал страницу регистрации, частично довыпиливал материал

--- front/src/app/views/users/registration-page/registration-page.component.ts
+++ front/src/app/views/users/registration-page/registration-page.component.ts
@@ -1,12 +1,12 @@
-import {Component} from '@angular/core';
-import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {NgIf, NgStyle} from '@angular/common';
-import {Router, RouterLink} from '@angular/router';
-import {HttpErrorResponse} from '@angular/common/http';
-import {MatSnackBar} from '@angular/material/snack-bar';
-import {AuthService} from '@core/auth';
-import {DefaultResponseType, LoginResponseType, SignupResponseType} from '@shared/types';
-
+import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf, NgStyle } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AuthService } from '@core/auth';
+import { DefaultResponseType, LoginResponseType } from '@shared/types';
+import { MessageService, ToastMessageOptions } from 'primeng/api';
+import { SnackStatusesUtil } from '@shared/utils';
 
 @Component({
   selector: 'app-registration-page',
@@ -16,11 +16,12 @@ import {DefaultResponseType, LoginResponseType, SignupResponseType} from '@share
     NgIf,
     ReactiveFormsModule,
     RouterLink,
-    NgStyle
+    NgStyle,
   ],
-  templateUrl: './registration-page.component.html'
+  templateUrl: './registration-page.component.html',
 })
 export class RegistrationPageComponent {
+  private _status: ToastMessageOptions = {} as ToastMessageOptions;
 
   protected _signupForm = this._fb.group({
     password: ['', [Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,12})$'), Validators.required]],
@@ -28,13 +29,13 @@ export class RegistrationPageComponent {
     phone: ['', [Validators.pattern('^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$')]],
     name: ['', [Validators.required]],
     lastName: ['', [Validators.required]],
-    accept: [false, [Validators.requiredTrue]]
+    accept: [false, [Validators.requiredTrue]],
   });
 
   constructor(private _authService: AuthService,
               private _router: Router,
               private _fb: FormBuilder,
-              private _snackBar: MatSnackBar) {
+              private _snackBar: MessageService) {
   }
 
   get username() {
@@ -60,35 +61,30 @@ export class RegistrationPageComponent {
   protected _signup() {
     if (this._signupForm.valid && this._signupForm.value) {
       this._authService.signup({
-          username: this._signupForm.value.username!,
-          password: this._signupForm.value.password!.toString(),
-          name: this._signupForm.value.name!,
-          phoneNumber:  this._signupForm.value.phone?.toString() ?? '',
-          lastName: this._signupForm.value.lastName!,
-        },
-      )
+        username: this._signupForm.value.username!,
+        password: this._signupForm.value.password!.toString(),
+        name: this._signupForm.value.name!,
+        phoneNumber: this._signupForm.value.phone?.toString() ?? '',
+        lastName: this._signupForm.value.lastName!,
+      })
         .subscribe({
-          next: (data: DefaultResponseType | SignupResponseType) => {
-
-            let error = null;
-            if ((data as DefaultResponseType).error !== undefined) {
-              error = (data as DefaultResponseType).message;
-              console.log(error);
-            }
-
+          error: err => {
+            this._status = SnackStatusesUtil.getStatuses('error', 'Ошибка при регистрации');
+            this._snackBar.add(this._status);
+            console.log(err);
+          },
+          complete: () => {
             this._authService.login(this._signupForm.value.username!, this._signupForm.value.password!)
               .subscribe({
                 next: (data: LoginResponseType | DefaultResponseType) => {
-                  if ((data as DefaultResponseType).error !== undefined) {
-                    this._snackBar.open('Ошибка при авторизации');
-                    throw new Error((data as DefaultResponseType).message );
-                  }
                   if (data as LoginResponseType) {
                     const loginResponse = data as LoginResponseType;
                     if (!loginResponse.access_token) {
-                      this._snackBar.open('Что-то пошло не так');
+                      this._status = SnackStatusesUtil.getStatuses('error', 'Ошибка при авторизации');
+                      this._snackBar.add(this._status);
+                      throw new Error('Ошибка при авторизации');
                     } else {
-                      this._authService.setTokens(loginResponse.access_token,);
+                      this._authService.setTokens(loginResponse.access_token);
                       this._authService.setUserInfo({
                         userId: loginResponse.userId,
                         phoneNumber: loginResponse.phoneNumber,
@@ -99,18 +95,12 @@ export class RegistrationPageComponent {
                   }
                 },
                 error: (error: HttpErrorResponse) => {
-                  this._snackBar.open('Ошибка при авторизации');
+                  this._status = SnackStatusesUtil.getStatuses('error', 'Ошибка при авторизации');
+                  this._snackBar.add(this._status);
                   throw new Error(error.error.message);
-                }
+                },
               });
           },
-          error: (errorResponse: HttpErrorResponse) => {
-            if (errorResponse.error && errorResponse.message) {
-              this._snackBar.open(errorResponse.message);
-            } else {
-              this._snackBar.open('Ошибка регистрации');
-            }
-          }
         });
     }
   }
