5f4afab3936d7c0c13a0f000135f6ddae565a6ae
sergey56431 <sergey56431@gmail.com>
2025-03-10 21:04:10 +0600

    попытка сделать установку приложения через уведомления и начало работы над кэшированием

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
-  RouterLink,
   RouterOutlet,
 } from '@angular/router';
 import { routes } from './app.routes';
@@ -12,7 +11,7 @@ import { Toast } from 'primeng/toast';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, AppStoreModule, Toast],
+  imports: [RouterOutlet, AppStoreModule, Toast],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -20,8 +19,55 @@ export class AppComponent implements OnInit {
   title = 'Ногти.Love';
   protected readonly routes = routes;
   protected readonly ActivatedRouteSnapshot = ActivatedRouteSnapshot;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  private installPromptEvent;
 
-  constructor(private primeng: PrimeNG) {}
+  constructor(private primeng: PrimeNG) {
+    this.checkIfInstalled();
+
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./ngsw-work er.json').then(registration => {
+          console.log('Service Worker зарегистрирован:', registration);
+        }).catch(error => {
+          console.error('Ошибка регистрации Service Worker:', error);
+        });
+      });
+    }
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+      // Предотвращаем отображение стандартного диалогового окна
+      event.preventDefault();
+      this.installPromptEvent = event;
+      this.showInstallPrompt();
+    });
+  }
+
+  checkIfInstalled() {
+    // Проверка, если приложение открыто в режиме PWA
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      return;
+    }
+  }
+
+  showInstallPrompt() {
+    // Используем confirm для предложения установки
+    const userConfirmed = confirm('Хотите установить это приложение на свое устройство?');
+
+    if (userConfirmed && this.installPromptEvent) {
+      this.installPromptEvent.prompt();
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      this.installPromptEvent.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('Пользователь принял установку приложения');
+        } else {
+          console.log('Пользователь отклонил установку приложения');
+        }
+      });
+    }
+  }
 
   ngOnInit() {
     this.primeng.ripple.set(true);
